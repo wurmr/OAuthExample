80142bebcfa85b354257de4972ad3cf7dfc67561
Jim <jkarg@ikarg.com>
2014-11-18 00:15:28 -0600

    Added SAML2 token parsing
    Commented out code also shows how to do verification in theory.

--- authentication.js
+++ authentication.js
@@ -1,10 +1,61 @@
 'use strict';
 
+var saml = require('saml20');
+
 module.exports = {
   ensureAuthenticated: function(req, res, next) {
     if (req.isAuthenticated()) {
       return next();
     }
     return res.send(401);
+  },
+
+  ensureSaml2: function(req, res, next) {
+    var authHeader = req.headers.authorization;
+
+    if (!authHeader || authHeader.indexOf('Bearer') == -1) {
+      res.send(403, {
+        message: "Please send me a token!"
+      });
+      return;
+    };
+
+    var token = (new Buffer(authHeader.slice('Bearer '.length, authHeader.length - 1), 'base64')).toString();
+
+    if (!token) {
+      res.send(403, {
+        message: "Please send me a token!"
+      });
+      return
+    };
+
+    /** Validation Code, Works if certs are signed correctly */
+
+
+    saml.parse(token, function(err, profile) {
+      req.user = profile.claims;
+      next();
+    });
+
+    /**
+    var options = {
+      //thumbprint: '3648D5858403AD0094D8EC051F0497A3'
+      //publicKey: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA0sjIJw7Drb7ra0HTWE7N/MBGfMN/AzULjg+rQwib7RjKozxvFq97MJPPsRUDLsd9l4of8oivGYKtnnO1Mp1TDGtLqTnuQMe0PM+4dariXsk+UK1TPSHFoxsAV1BLRCJHOqOfnuirML6TLx7H9n7GwmymSJRuG9ty9BYC/V4OfIkAJXA3JF1uXFzX3ElueLCr4FroEh11ZfVI4CLAWl3jQx1fmkqC8Fufn9sKV3ui8RBA1RirK4R4XB/gUyDQ/nIZnpnMv46F312OUjcMgRg8pCkkc/ww7YMENwVCRPfKxx7IJ2wkMxwpU8MiLCiJJasU/rDknAiRsk+Ndw4TcXVU1wIDAQAB'
+      //audince: 'https://localhost:3000'
+    };
+
+    saml.validate(token, options, function(err, profile) {
+      if (err) {
+        res.send(403, {
+          message: err.message
+        });
+        return;
+      }
+
+      req.user = profile.claims;
+      next();
+    });
+*/
+
   }
 };
--- routes/user.js
+++ routes/user.js
@@ -2,10 +2,15 @@ var express = require('express');
 var router = express.Router();
 var authentication = require('../authentication');
 var passport = require('passport');
+var saml = require('saml20');
 
 router.get('/me', passport.authenticate('google-token', {
   session: false
 }), function(req, res) {
+  res.send(req.user);
+});
+
+router.post('/me/saml', authentication.ensureSaml2, function(req, res) {
   res.send(req.user);
 });
 
