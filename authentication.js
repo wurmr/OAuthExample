9cec0fc21f131e65fe00a7c83e907c5467133713
Jim <jkarg@ikarg.com>
2014-11-18 23:30:02 -0600

    Working SAML assertion checks

--- authentication.js
+++ authentication.js
@@ -28,23 +28,12 @@ module.exports = {
       });
       return
     };
-/*
-    saml.parse(token, function(err, profile) {
-      if (err) {
-        res.send(403, { message: err.message });
-        return;
-      }
 
-      req.user = profile.claims;
-      next();
-    });
-*/
-    /** Validation Code, Works if certs are signed correctly */
-
+    /** Validation Code, This verifies the assertion is legit */
     var options = {
-      //thumbprint: '283db4bd92c84fc2b4ca7aead347850aefd1bf82'
-      publicKey: 'MIIBCgKCAQEA0sjIJw7Drb7ra0HTWE7N/MBGfMN/AzULjg+rQwib7RjKozxvFq97MJPPsRUDLsd9l4of8oivGYKtnnO1Mp1TDGtLqTnuQMe0PM+4dariXsk+UK1TPSHFoxsAV1BLRCJHOqOfnuirML6TLx7H9n7GwmymSJRuG9ty9BYC/V4OfIkAJXA3JF1uXFzX3ElueLCr4FroEh11ZfVI4CLAWl3jQx1fmkqC8Fufn9sKV3ui8RBA1RirK4R4XB/gUyDQ/nIZnpnMv46F312OUjcMgRg8pCkkc/ww7YMENwVCRPfKxx7IJ2wkMxwpU8MiLCiJJasU/rDknAiRsk+Ndw4TcXVU1wIDAQAB'
-      //audince: 'https://localhost:3000'
+      thumbprint: '283db4bd92c84fc2b4ca7aead347850aefd1bf82',
+      //publicKey: 'MIIC4DCCAcigAwIBAgIQSIBI0oWWDLVFDbTfNy0PiDANBgkqhkiG9w0BAQsFADAsMSowKAYDVQQDEyFBREZTIFNpZ25pbmcgLSBhZGZzLmludGVydGVjaC5jb20wHhcNMTQxMDMwMTkyMTQyWhcNMTUxMDMwMTkyMTQyWjAsMSowKAYDVQQDEyFBREZTIFNpZ25pbmcgLSBhZGZzLmludGVydGVjaC5jb20wggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDSyMgnDsOtvutrQdNYTs38wEZ8w38DNQuOD6tDCJvtGMqjPG8Wr3swk8+xFQMux32Xih/yiK8Zgq2ec7UynVMMa0upOe5Ax7Q8z7h1quJeyT5QrVM9IcWjGwBXUEtEIkc6o5+e6KswvpMvHsf2fsbCbKZIlG4b23L0FgL9Xg58iQAlcDckXW5cXNfcSW54sKvgWugSHXVl9UjgIsBaXeNDHV+aSoLwW5+f2wpXe6LxEEDVGKsrhHhcH+BTIND+chmemcy/joXfXY5SNwyBGDykKSRz/DDtgwQ3BUJE98rHHsgnbCQzHClTwyIsKIklqxT+sOScCJGyT413DhNxdVTXAgMBAAEwDQYJKoZIhvcNAQELBQADggEBAC/ryI8rBB15Acb4uHq4piXHhRZHP+6pwtM1KYtod1oewI4yedFamgRqLFUn0wyPz5INd0qcqg45OVSYDt+kDkZki/bXeY0Fq4BG87LBmvoxda0FifdBO/g2xq6ajnObXnercEu+2y517dfUQumTMoSo74vV+f6pGSXxxebsvZTRPqN6BWYamx/zmrwW51l8QAH4bjKqdzgIT+/L9UJsU6fqrVJFUMbVyojcrmlVNZWxYchBcVxCDOJqV4OCugqOXgxepPjyvoZPmqXTL0OftLTF1BYWCG+jlpFaeVxL29maCg+sh4jWgK6jRsuUbVDwWOPoJDHTbWvmZ+GWGtKKyzg='
+      audience: 'https://localhost:3000/'
     };
 
     saml.validate(token, options, function(err, profile) {
@@ -58,6 +47,5 @@ module.exports = {
       req.user = profile.claims;
       next();
     });
-
   }
 };
